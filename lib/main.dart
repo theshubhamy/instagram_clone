6419b9e1293f0bfeb85225b96c3515aa3afb9c31
theshubhamy <devshubhamyadav@gmail.com>
2023-04-08 06:35:54 +0530

    shubham

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:usocial/layouts/mobile_screen_layout.dart';
-import 'package:usocial/layouts/responsive_layout.dart';
-import 'package:usocial/layouts/web_screen_layout.dart';
+// auto refresh
+import 'package:auto_reload/auto_reload.dart';
+
+// import 'package:usocial/layouts/mobile_screen_layout.dart';
+// import 'package:usocial/layouts/responsive_layout.dart';
+// import 'package:usocial/layouts/web_screen_layout.dart';
+import 'package:usocial/screens/login.dart';
 import 'package:usocial/utils/colors.dart';
 import 'package:usocial/firebase_options.dart';
 
@@ -24,9 +28,10 @@ class MyApp extends StatelessWidget {
       title: 'Instagram Clone',
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: const ResponsiveLayout(
-          webScreenLayout: WebScreenLayout(),
-          mobileScreenLayout: MobileScreenLayout()),
+      // home: const ResponsiveLayout(
+      //     webScreenLayout: WebScreenLayout(),
+      //     mobileScreenLayout: MobileScreenLayout()),
+      home: const Login(),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:usocial/utils/colors.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: SafeArea(
+            child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: Container(),
+                    ),
+                    SvgPicture.asset(
+                      "assets/images/instagram.svg",
+                      colorFilter:
+                          const ColorFilter.mode(primaryColor, BlendMode.srcIn),
+                      height: 64,
+                    ),
+                    const SizedBox(
+                      height: 64,
+                    )
+                  ],
+                ))));
   }
 }
